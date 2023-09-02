cbcbf304b67a014eab0707b4454dadba4c4bc6d2
brahimEzzyouy <b.ezzyouy@etu.enset-media.ac.ma>
2023-09-02 12:48:49 +0100

    last update

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # FullStack-App-pringBoot-React
+
+the main goal of this application is to practice Spring boot & React technologie
+
+## Screens
+### backend
+![](./cap2.png)
+### frontend
+![](./cap.png)
+### database
+![](./cap3.png)
+
+
--- userfrontend/src/component/User.js
+++ userfrontend/src/component/User.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import TextField from '@mui/material/TextField'
-import { Button, Paper } from '@mui/material'
+import { Button, Container, Paper } from '@mui/material'
 import Grid from '@mui/material/Unstable_Grid2'
 import { createTheme, ThemeProvider, styled } from '@mui/material/styles'
 
@@ -13,57 +13,88 @@ const StyledContainer = styled('div')({
 export default function User () {
   const paperStyles = { padding: '50px 20px', width: 600, margin: '20px auto' }
   const [name, setName] = useState('')
-	const [address, setAddress] = useState('')
-	
-	const handleClick = (e) => {
-		e.preventDefault()
-		const user = { name, address }
-		console.log(user);
-	}
-  return (
-    <Paper elevation={3} style={paperStyles}>
-      <h1 style={{ color: 'blue' }}>
-        <u>Add User</u>
-      </h1>
-      <StyledContainer>
-        <Grid
-          container
-          direction={'column'}
-          spacing={3}
-          columnGap={2}
-          rowSpacing={2}
-          columnSpacing={1}
-        >
-          <form noValidate autoComplete='off'>
-            <Grid item>
-              <TextField
-                fullWidth
-                id='outlined-basic'
-                label='User Name'
-                variant='outlined'
-                value={name}
-                onChange={e => setName(e.target.value)}
-              />
-            </Grid>
+  const [address, setAddress] = useState('')
+  const [user, setUser] = useState([])
 
-            <Grid item>
-              <TextField
-                fullWidth
-                id='outlined-basic'
-                label='User Address'
-                variant='outlined'
-                value={address}
-                onChange={e => setAddress(e.target.value)}
-              />
-            </Grid>
-            <Grid item>
-              <Button variant='contained' onClick={handleClick}>
-                Add
-              </Button>
-            </Grid>
-          </form>
-        </Grid>
-      </StyledContainer>
-    </Paper>
+  const handleClick = e => {
+    e.preventDefault()
+    const user = { name, address }
+    console.log(user)
+    fetch('http://localhost:8080/user/add', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(user)
+    }).then(() => {
+      console.log('new Student added')
+    })
+  }
+  useEffect(() => {
+    fetch('http://localhost:8080/user/getAll')
+      .then(res => res.json())
+      .then(result => {
+        setUser(result)
+      })
+  }, [])
+
+  return (
+    <Container>
+      <Paper elevation={3} style={paperStyles}>
+        <h1 style={{ color: 'blue' }}>
+          <u>Add User</u>
+        </h1>
+        <StyledContainer>
+          <Grid
+            container
+            direction={'column'}
+            spacing={3}
+            columnGap={2}
+            rowSpacing={2}
+            columnSpacing={1}
+          >
+            <form noValidate autoComplete='off'>
+              <Grid item>
+                <TextField
+                  fullWidth
+                  id='outlined-basic'
+                  label='User Name'
+                  variant='outlined'
+                  value={name}
+                  onChange={e => setName(e.target.value)}
+                />
+              </Grid>
+
+              <Grid item>
+                <TextField
+                  fullWidth
+                  id='outlined-basic'
+                  label='User Address'
+                  variant='outlined'
+                  value={address}
+                  onChange={e => setAddress(e.target.value)}
+                />
+              </Grid>
+              <Grid item>
+                <Button variant='contained' onClick={handleClick}>
+                  Add
+                </Button>
+              </Grid>
+            </form>
+          </Grid>
+        </StyledContainer>
+      </Paper>
+      <Paper elevation={3} style={paperStyles}>
+        {user.map(user => (
+          <Paper
+            elevation={6}
+            style={{ margin: '10px', padding: '15px', textAlign: 'left' }}
+            key={user.id}
+          >
+            Id:{user.id}<br/>
+            Name:{user.name}<br/>
+            Address:{user.address}
+          </Paper>
+        ))}
+      </Paper>
+    </Container>
   )
 }
